356046de76bff8177a1172716847ab321e154ee9
Ananya Kumari <71487043+kriananya@users.noreply.github.com>
2021-08-19 12:06:20 +0530

    Add files via upload

---
+++ DAO.java
@@ -0,0 +1,28 @@
+package javaProject;
+
+import java.sql.*;
+public class DAO {
+	
+		public static Connection connect()
+		{
+			Connection conn=null;
+			try
+			{
+				Class.forName("oracle.jdbc.driver.OracleDriver");
+				conn=DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe","aec","1234");
+			
+		}catch(ClassNotFoundException e)
+		{
+			e.printStackTrace();
+			
+		}
+		catch(SQLException e)
+		{
+			e.printStackTrace();
+			
+			
+	}
+		return conn;
+
+}
+}
